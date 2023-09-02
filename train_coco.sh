6477b0f2ead145dbafdd4cd70e739d4f62526bec
naye0319 <naye03@naver.com>
2023-09-03 03:41:19 +0900

    as

--- train_coco.sh
+++ train_coco.sh
@@ -1,6 +1,6 @@
 python3 train.py \
 --data_name coco --cnn_type resnet152 --wemb_type glove \
---margin 0.1 --max_violation --img_num_embeds 4 --txt_num_embeds 4 \
+--margin 0.2 --max_violation --img_num_embeds 4 --txt_num_embeds 4 \
 --img_attention --txt_attention --img_finetune --txt_finetune \
 --mmd_weight 0.01 --unif_weight 0.01 \
 --batch_size 200 --warm_epoch 0 --num_epochs 80 \
