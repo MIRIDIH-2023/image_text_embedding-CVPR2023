4da17aaee0d35fea35c8dbffb7531e68b37c14a2
naye0319 <naye03@naver.com>
2023-09-04 02:07:18 +0900

    sa

--- custom_tokenizer_vocab.py
+++ custom_tokenizer_vocab.py
@@ -1,24 +1,31 @@
-from transformers import BertTokenizer , BertModel
+from transformers import BertTokenizer , BertTokenizerFast ,BertModel
 from vocab import Vocabulary
 
 if __name__ == "__main__":
     #tokenizer = BertTokenizer.from_pretrained('monologg/kobigbird-bert-base')
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    bert = BertModel.from_pretrained('bert-base-uncased')
+    #tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    #bert = BertModel.from_pretrained('bert-base-uncased')
+    #tokenizer = BertTokenizerFast.from_pretrained("kykim/bert-kor-base")
+    tokenizer = BertTokenizerFast.from_pretrained("kykim/bertshared-kor-base")
     
-    """
     vocab = Vocabulary()
     for keys, idx in tokenizer.vocab.items():
         vocab.add_word(keys)
-    vocab.replace('##：','<mask>') #三 上 下 不 丑
-    vocab.replace('##？','<pad>')
-    vocab.replace('##～','<start>')
-    vocab.replace('##／','<end>')
-    vocab.replace('##．','<unk>')
+    vocab.replace('[unused100]','<mask>') #三 上 下 不 丑
+    vocab.replace('[unused101]','<pad>')
+    vocab.replace('[unused102]','<start>')
+    vocab.replace('[unused103]','<end>')
+    vocab.replace('[unused104]','<unk>')
     
     print(vocab.word2idx)
+    
+    #for keys , id in vocab.word2idx.items():
+    #    print(keys, id)
+    #    if(id==2000):
+    #        break
+    
     print(tokenizer.all_special_tokens)
     print(vocab.idx)
     print(tokenizer.vocab_size)
-    """
-    print(bert)
+    
+    #print(bert)
